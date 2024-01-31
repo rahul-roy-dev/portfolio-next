f355578c87f8b488950e99bf143950dffdcd5888
Rahul Roy <lanoire141018@gmail.com>
2024-01-31 17:53:41 +0700

    update about page

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,17 @@ import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary/60 h-full">
+    <div className="h-[100%]">
       {/* text */}
-      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+      <div className="w-full h-full ">
+        <div className="text-center flex flex-col justify-center  xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h1"
+            className="h1 md:text-[50px]"
           >
             If you can imagine it <br /> I can{" "}
             <span className="text-accent">Code it</span>
